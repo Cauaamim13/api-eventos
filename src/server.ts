1417edc2bdcf3b2af06817e3f0057cfec2d01d1a
CauaAmim <cauaamim@gmail.com>
2026-03-26 23:09:29 -0300

    add rota post/insert e cadastro de cliente

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,49 @@
 import express, { type Request, type Response } from 'express';
+import db from './database/connection.js';
 
 const app = express();
 app.use(express.json());
 
-app.get('/teste', (req: Request, res: Response) => {
-    return res.json({ 
-        mensagem: "Fala! Nossa API está no ar!",
-        status: "Sucesso"
+
+app.get('/banco', async (req: Request, res: Response) => {
+    try {
+        const [clientes] = await db.query('SELECT * FROM clientes');
+        return res.json({ 
+            mensagem: "Conexão com o MySQL feita com sucesso!",
+            dados: clientes 
+        });
+    } catch (erro) {
+        console.error("Deu ruim no banco:", erro);
+        return res.status(500).json({ erro: "Falha na conexão com o banco" });
+    }
+});
+
+
+app.post('/clientes', async(req:Request, res: Response) =>
+{
+    try {
+        const { nome_completo, cpf, telefone, email } = req.body;
+  
+
+    if (!nome_completo) {
+        return res.status(400).json ({erro: "O nome completo é obrigatório!"});
+    }
+
+    const comandosSQL = 'INSERT into clientes (nome_completo, cpf, telefone, email) VALUES (?,?,?,?)';
+    const dados = [nome_completo, cpf, telefone, email];
+
+
+    const [resultado] = await db.query(comandosSQL, dados);
+
+    return res.status(201).json({
+        mensagem: "Cliente cadastrado com sucesso!",
+        resultado
     });
+
+} catch (erro) {
+    console.error ("Erro ao cadastrar o cliente: ", erro);
+    return res.status(500).json({erro: "Falha no servidor"});
+}
 });
 
 const PORTA = 3333;
